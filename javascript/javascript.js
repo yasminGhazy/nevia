f9d8ad01b8e6402546425f51eac4504a03bbc713
yasminGhazy <jesygzaki@gmail.com>
2020-03-20 01:10:21 +0200

    n

---
+++ javascript/javascript.js
@@ -0,0 +1,19 @@
+/*$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})*/
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel();
+  });
