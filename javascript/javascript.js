00b8f146bc3f7f5493fa1b0b2a01139851157b23
yasminGhazy <jesygzaki@gmail.com>
2020-03-23 22:03:24 +0200

    n

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,7 +2,11 @@ $('.owl-carousel').owlCarousel({
     loop:false,
     margin:10,
     nav:true,
-
+    navText: [
+        '<i class="fa fa-fw fa-chevron-left" ></i>',
+        '<i class="fa fa-fw fa-chevron-right" ></i>'
+    ],
+dots:false,
     responsive:{
         0:{
             items:1
@@ -10,16 +14,32 @@ $('.owl-carousel').owlCarousel({
         700:{
             items:2
         },
-        1000:{
+        1020:{
             items:3
         }
     }
 
   
-})
- $(".next").click(function(){
-    owl.trigger('owl.next');
-  })
-  $(".prev").click(function(){
-    owl.trigger('owl.prev');
-  })
+});
+/*$(document).ready(function() {
+
+ 
+    var owl = $("owl-slider");
+   
+    owl.owlCarousel({
+        items : 10, //10 items above 1000px browser width
+        itemsDesktop : [1000,5], //5 items between 1000px and 901px
+        itemsDesktopSmall : [900,3], // betweem 900px and 601px
+        itemsTablet: [600,2], //2 items between 600 and 0
+        itemsMobile : false // itemsMobile disabled - inherit from itemsTablet option
+    });
+   
+    // Custom Navigation Events
+    $(".next").click(function(){
+      owl.trigger('owl.next');
+    })
+    $(".prev").click(function(){
+      owl.trigger('owl.prev');
+    })
+ 
+  });*/
